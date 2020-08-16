16cf161efd0d06fe5a06a67eb0c2cf854c0b1031
Carlos DAgostino <dagsis@dagsis.com.ar>
2020-08-16 13:46:28 -0300

    Inicio

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+   public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Name { get; set; }
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(t => t.Name).IsUnique();
+
+        }
+
+    }
+
+}
